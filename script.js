55c7e0d11abd51d6dea34855cf77f303e6328966
Annemarie Sheridan <atsheridan38@gmail.com>
2022-10-24 18:20:44 -0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMM Do, YYYY"));
 
-var TimeblocksEl = document.querySelector("Timeblocks") 
-TimeblocksEl = moment()
+var timeblocksEl = document.querySelectorAll(".description")
+var saveButtonsEl = document.querySelectorAll(".saveBtn")
+console.log(saveButtonsEl)
 moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+console.log(timeblocksEl[0].getAttribute("data-whatever"))
+$(".time-block").each(function
+    (index) {
+        saveButtonsEl[index].addEventListener("click",function(){
+            console.log(timeblocksEl[index].value)
+            localStorage.setItem(key, value);
+        })
+  
+
+
+    // console.log(timeblocksEl[])
+    if (moment().format("H") == timeblocksEl[index].getAttribute("data-hour")) {
+        timeblocksEl[index].setAttribute("class", "description present")
+    } else if (moment().format("H") < timeblocksEl[index].getAttribute("data-hour")) {
+        timeblocksEl[index].setAttribute("class", "description future")
+    } else {
+        timeblocksEl[index].setAttribute("class", "description past")
+    }
+});
+
+
+
+
